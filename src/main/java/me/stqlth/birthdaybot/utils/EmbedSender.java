abe9e346c1f0efa568ebbf0c7f72581a267db340
stqlth <stqlth@gmail.com>
2020-04-21 14:16:34 -0400

    Updated version

---
+++ src/main/java/me/stqlth/birthdaybot/utils/EmbedSender.java
@@ -0,0 +1,29 @@
+package me.stqlth.birthdaybot.utils;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.awt.*;
+
+public class MessageBuilder {
+
+	public static void sendEmbed(@Nonnull TextChannel channel, @Nullable String title, @Nonnull String description, @Nullable Color color) {
+		EmbedBuilder builder = new EmbedBuilder();
+		if (color == null) color = Color.decode("5AE8FF"); //Default color
+		builder.setDescription(description)
+				.setColor(color);
+		if (title != null) builder.setTitle(title);
+
+
+		channel.sendMessage(builder.build()).queue(null, error -> {
+			if (!(error instanceof InsufficientPermissionException)) {
+				Logger.Error("Failed to send message to a TextChannel with ID: " + channel.getId(), error);
+			}
+		});
+
+	}
+}
