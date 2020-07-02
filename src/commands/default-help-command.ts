815ebe82b38b3194de4a5eddd02a8855ad6ae8b7
Kevin Novak <kevinmnovak@yahoo.com>
2020-07-01 23:40:03 -0400

    Import ordering

--- src/commands/default-help-command.ts
+++ src/commands/default-help-command.ts
@@ -1,7 +1,7 @@
 import { DMChannel, Message, MessageEmbed, TextChannel } from 'discord.js';
 
-import { Command } from './command';
 import { MessageUtils } from '../utils';
+import { Command } from './command';
 
 let Config = require('../../config/config.json'); // Possible support for server specific prefixes?
 
