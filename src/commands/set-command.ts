3e36d8f84ad6f821bbf4db78001515b666fa1bbd
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-07 16:08:45 -0400

    Implemented new collector system and fixed some invalid dates

--- src/commands/set-command.ts
+++ src/commands/set-command.ts
@@ -1,4 +1,12 @@
 import * as Chrono from 'chrono-node';
+
+import { ActionUtils, FormatUtils, PermissionUtils } from '../utils';
+import {
+    CollectOptions,
+    CollectorUtils,
+    ExpireFunction,
+    MessageFilter,
+} from 'discord.js-collector-utils';
 import {
     DMChannel,
     Message,
@@ -9,11 +17,15 @@ import {
     User,
 } from 'discord.js';
 
-import { UserRepo } from '../services/database/repos';
-import { ActionUtils, FormatUtils, PermissionUtils } from '../utils';
 import { Command } from './command';
+import { UserRepo } from '../services/database/repos';
 
 let Config = require('../../config/config.json');
+
+const COLLECT_OPTIONS: CollectOptions = {
+    time: Config.promptExpireTime * 1000,
+    reset: true,
+};
 
 export class SetCommand implements Command {
     public name: string = 'set';
@@ -26,6 +38,17 @@ export class SetCommand implements Command {
     constructor(private userRepo: UserRepo) {}
 
     public async execute(args: string[], msg: Message, channel: TextChannel | DMChannel) {
+        let stopFilter: MessageFilter = (nextMsg: Message) =>
+            nextMsg.author.id === msg.author.id &&
+            nextMsg.content.split(' ')[0].toLocaleLowerCase() === 'bday';
+        let expireFunction: ExpireFunction = async () => {
+            await channel.send(
+                new MessageEmbed()
+                    .setTitle('Birthday Setup - Expired')
+                    .setDescription('Type `bday set` to rerun the setup.')
+                    .setColor(Config.colors.error)
+            );
+        };
         let target: User;
 
         if (args.length === 3) {
@@ -75,6 +98,9 @@ export class SetCommand implements Command {
         let birthday: string;
         let timeZone: string;
 
+        let month: number; // Get the numeric value of month
+        let day: number;
+
         if (userData) {
             // Are they in the database?
             if (userData.ChangesLeft === 0) {
@@ -109,30 +135,17 @@ export class SetCommand implements Command {
         if (suggest) timeZoneEmbed.setTitle(`Setup For ${target.username} - Time Zone`);
         else timeZoneEmbed.setTitle('User Setup - Time Zone');
 
-        await channel.send(timeZoneEmbed);
+        let timezoneMessage = await channel.send(timeZoneEmbed);
 
-        let collector = channel.createMessageCollector(
+        timeZone = await CollectorUtils.collectByMessage(
+            msg.channel,
+            // Collect Filter
             (nextMsg: Message) => nextMsg.author.id === msg.author.id,
-            { time: Config.promptExpireTime * 1000 }
-        );
-
-        collector.on('collect', async (nextMsg: Message) => {
-            // Check if bot has permission to send a message
-            if (!PermissionUtils.canSend(channel)) {
-                collector.stop();
-                return;
-            }
-
-            // Check if another command was ran, if so cancel the current process
-            let nextMsgArgs = nextMsg.content.split(' ');
-            if (nextMsgArgs[0]?.toLowerCase() === 'bday') {
-                collector.stop();
-                return;
-            }
-
-            if (!timeZone) {
-                let timeZoneInput = FormatUtils.findZone(nextMsg.content); // Try and get the time zone
-                if (!timeZoneInput) {
+            stopFilter,
+            // Retrieve Result
+            async (nextMsg: Message) => {
+                let input = FormatUtils.findZone(nextMsg.content); // Try and get the time zone
+                if (!input) {
                     let embed = new MessageEmbed()
                         .setDescription('Invalid time zone!')
                         .setFooter(`Please check above and try again!`, msg.client.user.avatarURL())
@@ -144,28 +157,40 @@ export class SetCommand implements Command {
                     return;
                 }
 
-                timeZone = timeZoneInput;
+                return input;
+            },
+            expireFunction,
+            COLLECT_OPTIONS
+        );
 
-                collector.resetTimer(); // Reset timer
+        ActionUtils.deleteMessage(timezoneMessage);
 
-                let birthdayEmbed = new MessageEmbed()
-                    .setDescription(
-                        `\n\Now, please provide ${msg.client.user.toString()} with your birth month and day [(?)](https://birthdaybot.scottbucher.dev/faq#why-does-birthday-bot-only-need-my-birth-month-and-date)` +
-                            '\n\n**Example Usage** `08/28` (MM/DD)'
-                    )
-                    .setFooter(`This message expires in 2 minutes!`, msg.client.user.avatarURL())
-                    .setColor(Config.colors.default)
-                    .setTimestamp()
-                    .setAuthor(`${target.username}#${target.discriminator}`, target.avatarURL());
+        if (timeZone === undefined) {
+            return;
+        }
 
-                if (suggest) birthdayEmbed.setTitle(`Setup For ${target.username} - Birthday`);
-                else birthdayEmbed.setTitle('User Setup - Birthday');
+        let birthdayEmbed = new MessageEmbed()
+            .setDescription(
+                `\n\Now, please provide ${msg.client.user.toString()} with your birth month and day [(?)](https://birthdaybot.scottbucher.dev/faq#why-does-birthday-bot-only-need-my-birth-month-and-date)` +
+                    '\n\n**Example Usage** `08/28` (MM/DD)'
+            )
+            .setFooter(`This message expires in 2 minutes!`, msg.client.user.avatarURL())
+            .setColor(Config.colors.default)
+            .setTimestamp()
+            .setAuthor(`${target.username}#${target.discriminator}`, target.avatarURL());
 
-                await channel.send(birthdayEmbed);
-                return;
-            }
+        if (suggest) birthdayEmbed.setTitle(`Setup For ${target.username} - Birthday`);
+        else birthdayEmbed.setTitle('User Setup - Birthday');
 
-            if (!birthday) {
+        let birthdayMessage = await channel.send(birthdayEmbed);
+
+        birthday = await CollectorUtils.collectByMessage(
+            msg.channel,
+            // Collect Filter
+            (nextMsg: Message) => nextMsg.author.id === msg.author.id,
+            stopFilter,
+            // Retrieve Result
+            async (nextMsg: Message) => {
                 let results = await Chrono.parseDate(nextMsg.content); // Try an parse a date
 
                 if (!results) {
@@ -180,97 +205,109 @@ export class SetCommand implements Command {
                     return;
                 }
 
-                let month = results.getMonth() + 1; // Get the numeric value of month
-                let day = results.getDate();
-                birthday = `2000-${month}-${day}`;
+                month = results.getMonth() + 1; // Get the numeric value of month
+                day = results.getDate();
+                let temp = `2000-${month}-${day}`;
+                let doubleCheck = await Chrono.parseDate(temp);
 
-                let confirmationEmbed = new MessageEmbed().setColor(Config.colors.default);
-
-                if (suggest) {
-                    confirmationEmbed
-                        .setDescription(
-                            `${target.toString()}, please confirm this information is correct: **${FormatUtils.getMonth(
-                                month
-                            )} ${day}, ${timeZone}**`
-                        )
-                        .setFooter(
-                            `${target.username} has ${changesLeft} attempts left. By clicking confirm they will use one of them.`,
-                            msg.client.user.avatarURL()
-                        );
-                } else {
-                    confirmationEmbed
-                        .setDescription(
-                            `Please confirm this information is correct: **${FormatUtils.getMonth(
-                                month
-                            )} ${day}, ${timeZone}**`
-                        )
-                        .setFooter(
-                            `You have ${changesLeft} attempts left. By clicking confirm you will use one of them.`,
-                            msg.client.user.avatarURL()
-                        );
+                // Don't laugh at my double check it prevents the dates chrono misses on the first input
+                if (!doubleCheck) {
+                    let embed = new MessageEmbed()
+                        .setDescription('Invalid birthday!')
+                        .setFooter(`Please check above and try again!`, msg.client.user.avatarURL())
+                        .setTimestamp()
+                        .setColor(Config.colors.error);
+                    if (suggest) embed.setTitle(`Setup For ${target.username} - Birthday`);
+                    else embed.setTitle('User Setup - Birthday');
+                    await channel.send(embed);
+                    return;
                 }
 
-                let confirmationMsg = await channel.send(confirmationEmbed); // Send confirmation and emotes
-                await confirmationMsg.react(Config.emotes.confirm);
-                await confirmationMsg.react(Config.emotes.deny);
+                return temp;
+            },
+            expireFunction,
+            COLLECT_OPTIONS
+        );
 
-                collector.stop(); // Stop the message Collector (maybe rename collector to messageCollector)
+        ActionUtils.deleteMessage(birthdayMessage);
 
-                const filter = (nextReaction: MessageReaction, reactor: User) =>
-                    (nextReaction.emoji.name === Config.emotes.confirm ||
-                        nextReaction.emoji.name === Config.emotes.deny) &&
-                    nextReaction.users.resolve(msg.client.user.id) !== null &&
-                    reactor === target; // Reaction Collector Filter
+        if (birthday === undefined) {
+            return;
+        }
 
-                let reactionCollector = confirmationMsg.createReactionCollector(filter, {
-                    time: Config.promptExpireTime * 1000,
-                });
+        let confirmationEmbed = new MessageEmbed().setColor(Config.colors.default);
 
-                reactionCollector.on(
-                    'collect',
-                    async (nextReaction: MessageReaction, reactor: User) => {
-                        // Start Reaction Collector
-                        // Check if bot has permission to send a message
-                        if (!PermissionUtils.canSend(channel)) {
-                            reactionCollector.stop();
-                            return;
-                        }
-
-                        await ActionUtils.deleteMessage(confirmationMsg); // Try and delete the message
-
-                        if (nextReaction.emoji.name === Config.emotes.confirm) {
-                            // Confirm
-
-                            await this.userRepo.addOrUpdateUser(
-                                target.id,
-                                birthday,
-                                timeZone,
-                                changesLeft - 1
-                            ); // Add or update user
-
-                            let embed = new MessageEmbed()
-                                .setDescription(
-                                    `Successfully set your birthday to **${FormatUtils.getMonth(
-                                        month
-                                    )} ${day}, ${timeZone}**`
-                                )
-                                .setFooter(
-                                    `You now have ${changesLeft - 1} birthday attempts left.`
-                                )
-                                .setColor(Config.colors.success);
-                            await channel.send(embed);
-                            return;
-                        } else if (nextReaction.emoji.name === Config.emotes.deny) {
-                            // Cancel
-                            let embed = new MessageEmbed()
-                                .setDescription('Your birthday has not been set.')
-                                .setColor(Config.colors.error);
-                            await channel.send(embed);
-                            return;
-                        }
-                    }
+        if (suggest) {
+            confirmationEmbed
+                .setDescription(
+                    `${target.toString()}, please confirm this information is correct: **${FormatUtils.getMonth(
+                        month
+                    )} ${day}, ${timeZone}**`
+                )
+                .setFooter(
+                    `${target.username} has ${changesLeft} attempts left. By clicking confirm they will use one of them.`,
+                    msg.client.user.avatarURL()
                 );
-            }
-        });
+        } else {
+            confirmationEmbed
+                .setDescription(
+                    `Please confirm this information is correct: **${FormatUtils.getMonth(
+                        month
+                    )} ${day}, ${timeZone}**`
+                )
+                .setFooter(
+                    `You have ${changesLeft} attempts left. By clicking confirm you will use one of them.`,
+                    msg.client.user.avatarURL()
+                );
+        }
+
+        let trueFalseOptions = [Config.emotes.confirm, Config.emotes.deny];
+
+        let confirmationMessage = await channel.send(confirmationEmbed); // Send confirmation and emotes
+        for (let option of trueFalseOptions) {
+            await confirmationMessage.react(option);
+        }
+
+        let confirmation: string = await CollectorUtils.collectByReaction(
+            confirmationMessage,
+            // Collect Filter
+            (msgReaction: MessageReaction, reactor: User) =>
+                reactor.id === msg.author.id && trueFalseOptions.includes(msgReaction.emoji.name),
+            stopFilter,
+            // Retrieve Result
+            async (msgReaction: MessageReaction, reactor: User) => {
+                return msgReaction.emoji.name;
+            },
+            expireFunction,
+            COLLECT_OPTIONS
+        );
+
+        ActionUtils.deleteMessage(confirmationMessage);
+
+        if (confirmation === undefined) return;
+
+        if (confirmation === Config.emotes.confirm) {
+            // Confirm
+
+            await this.userRepo.addOrUpdateUser(target.id, birthday, timeZone, changesLeft - 1); // Add or update user
+
+            let embed = new MessageEmbed()
+                .setDescription(
+                    `Successfully set your birthday to **${FormatUtils.getMonth(
+                        month
+                    )} ${day}, ${timeZone}**`
+                )
+                .setFooter(`You now have ${changesLeft - 1} birthday attempts left.`)
+                .setColor(Config.colors.success);
+            await channel.send(embed);
+            return;
+        } else if (confirmation === Config.emotes.deny) {
+            // Cancel
+            let embed = new MessageEmbed()
+                .setDescription('Your birthday has not been set.')
+                .setColor(Config.colors.error);
+            await channel.send(embed);
+            return;
+        }
     }
 }
