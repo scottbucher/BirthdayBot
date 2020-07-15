3aefc77b877331be544e22a9d967fd897f8f3534
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-15 01:22:09 -0400

    Added try catch and more

--- src/api.ts
+++ src/api.ts
@@ -10,26 +10,31 @@ let Logs = require('../lang/logs.json');
 const app = express();
 
 export class Api {
-
     constructor(private userRepo: UserRepo) {}
 
     public async start(): Promise<void> {
         // Tell express to use body-parser's JSON parsing
         app.use(bodyParser.json());
 
+        // Get the votes
+        app.post('/votes', async (req, res) => {
+            try {
+                let voteData = new VoteData(req.body);
+
+                await this.userRepo.addUserVote('top.gg', voteData.UserDiscordId);
+            } catch (error) {
+                Logger.error(
+                    Logs.error.registeringVote.replace('{USER_ID}', req.body.UserDiscordId)
+                );
+            }
+        });
+
         // Voting Api Ready
-        app.get('/votes', (req, res) => {
-            res.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+        app.get('*', (req, res) => {
+            res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
         });
 
         // Voting Api has started
         app.listen(Config.apiPort, () => Logger.info(Logs.info.startedVotingApi));
-
-        // Get the votes
-        app.post('/votes', async (req, res) => {
-            let voteData = new VoteData(req.body);
-
-            await this.userRepo.addUserVote('top.gg', voteData.UserDiscordId);
-        });
     }
 }
