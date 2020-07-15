993ffa81424595c2842ebe45e3c35f72d6b8744d
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-14 23:44:18 -0400

    Updated vote model for data from the database

--- src/models/database/vote-models.ts
+++ src/models/database/vote-models.ts
@@ -1,17 +1,6 @@
-export class VoteData {
-    BotDiscordId: string;
+export interface Vote {
+    VoteId: number;
+    BotSiteName: string;
     UserDiscordId: string;
-    Type: string;
-    IsWeekend: boolean;D
-    Query: string;
-
-    constructor(
-        private body
-        ) {
-            this.BotDiscordId = body.bot;
-            this.UserDiscordId = body.user;
-            this.Type = body.type;
-            this.IsWeekend = body.IsWeekend;
-            this.Query = body.query;
-        }
+    VoteTime: string;
 }
