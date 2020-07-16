2593298489e981595f23b453a0ac9b815a239578
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-16 12:36:00 -0400

    Removed Logs

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -5,6 +5,5 @@ import { Logger } from './logger';
 export class HttpService {
     public async post(url: string, body: any, authorization: string): Promise<void> {
         let result = await rp.post(url, { body, json: true, headers: { Authorization: authorization } });
-        Logger.info(result);
     }
 }
