2d9c3e265798fd21dc491ddd2608cdcc42c9245c
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-16 12:36:26 -0400

    Removed unused import

--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -1,7 +1,5 @@
 import * as rp from 'request-promise';
 
-import { Logger } from './logger';
-
 export class HttpService {
     public async post(url: string, body: any, authorization: string): Promise<void> {
         let result = await rp.post(url, { body, json: true, headers: { Authorization: authorization } });
