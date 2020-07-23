47fcf702182d3d88f1eeb72c19f59628ef0c1716
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-23 00:10:30 -0400

    Improved flexibilty and output of testing command

--- src/commands/test-command.ts
+++ src/commands/test-command.ts
@@ -1,10 +1,11 @@
-import { DMChannel, Message, MessageEmbed, TextChannel } from 'discord.js';
-import moment from 'moment';
+import { DMChannel, Message, MessageEmbed, TextChannel, User } from 'discord.js';
 
-import { UserData } from '../models/database';
 import { BirthdayService } from '../services';
-import { GuildRepo } from '../services/database/repos';
 import { Command } from './command';
+import { GuildRepo } from '../services/database/repos';
+import { GuildUtils } from '../utils';
+import { UserData } from '../models/database';
+import moment from 'moment';
 
 let Config = require('../../config/config.json');
 
@@ -21,31 +22,42 @@ export class TestCommand implements Command {
 
     constructor(private birthdayService: BirthdayService, private guildRepo: GuildRepo) {}
 
-    public async execute(args: string[], msg: Message, channel: TextChannel | DMChannel) {
+    public async execute(args: string[], msg: Message, channel: TextChannel) {
         let guild = msg.guild;
 
         let guildData = await this.guildRepo.getGuild(msg.guild.id);
-        // Don't set a role for testing
-        guildData.BirthdayRoleDiscordId = null;
         // Mock message time to current hour
         guildData.MessageTime = moment().tz(MOCK_TIME_ZONE).hour();
+
+        let target: User;
+
+        if (args.length === 3) {
+            // Get who they are mentioning
+            target =
+                msg.mentions.members.first()?.user ||
+                GuildUtils.findMember(msg.guild, args[2])?.user;
+
+            // Did we find a user?
+            if (!target) {
+                let embed = new MessageEmbed()
+                    .setDescription('Could not find that user!')
+                    .setColor(Config.colors.error);
+                await channel.send(embed);
+                return;
+            }
+        } else {
+            // They didn't mention anyone
+            target = msg.client.user;
+        }
 
         // Mock user data for bot
         let userData: UserData = {
             Birthday: moment().toDate().toString(),
             ChangesLeft: 5,
             TimeZone: MOCK_TIME_ZONE,
-            UserDiscordId: msg.client.user.id,
+            UserDiscordId: target.id,
         };
 
-        this.birthdayService.celebrateBirthdays(guild, guildData, [userData], guild.members.cache);
-
-        let embed = new MessageEmbed()
-            .setTitle('Test Birthday Message')
-            .setDescription(
-                `A test birthday message was sent to the <#${guildData.BirthdayChannelDiscordId}> channel!\n\nIf you do not see a birthday message posted, please check that <@${msg.client.user.id}> has all required permissions in the birthday channel.\n\n[Join Support Server](https://discord.gg/9gUQFtz)`
-            )
-            .setColor(Config.colors.default);
-        await channel.send(embed);
+        this.birthdayService.celebrateBirthdays(guild, guildData, [userData], guild.members.cache, true, channel);
     }
 }
