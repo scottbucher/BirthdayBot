414704d04111553ce5381448c42144c5e821c422
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-07-29 22:07:04 -0400

    Logic if to prevent fetching users of a fully/mostly cached server

--- src/jobs/birthday-job.ts
+++ src/jobs/birthday-job.ts
@@ -1,11 +1,11 @@
-import { Client, Collection, Guild, GuildMember } from 'discord.js';
-import moment from 'moment';
-
-import { UserData } from '../models/database';
-import { BirthdayService, Logger } from '../services';
-import { GuildRepo, UserRepo } from '../services/database/repos';
 import { BdayUtils, MathUtils } from '../utils';
+import { BirthdayService, Logger } from '../services';
+import { Client, Collection, Guild, GuildMember } from 'discord.js';
+import { GuildRepo, UserRepo } from '../services/database/repos';
+
 import { Job } from './job';
+import { UserData } from '../models/database';
+import moment from 'moment';
 
 let Logs = require('../../lang/logs.json');
 
@@ -74,25 +74,28 @@ export class BirthdayJob implements Job {
             try {
                 let members: Collection<string, GuildMember>;
                 let beforeCacheSize = guild.members.cache.size;
-                try {
-                    members = await guild.members.fetch();
-                } catch (error) {
-                    members = guild.members.cache;
-                    Logger.error(
-                        Logs.error.birthdayService
-                            .replace('{GUILD_ID}', guildData.GuildDiscordId)
-                            .replace('{GUILD_NAME}', guild.name)
-                            .replace('{MEMBER_COUNT}', guild.memberCount.toLocaleString())
-                            .replace(
-                                '{MEMBER_CACHE_BEFORE_COUNT}',
-                                beforeCacheSize.toLocaleString()
-                            )
-                            .replace(
-                                '{MEMBER_CACHE_AFTER_COUNT}',
-                                guild.members.cache.size.toLocaleString()
-                            ),
-                        error
-                    );
+
+                if (!(members.size !== beforeCacheSize || members.size !== beforeCacheSize+1)) {
+                    try {
+                        members = await guild.members.fetch();
+                    } catch (error) {
+                        members = guild.members.cache;
+                        Logger.error(
+                            Logs.error.birthdayService
+                                .replace('{GUILD_ID}', guildData.GuildDiscordId)
+                                .replace('{GUILD_NAME}', guild.name)
+                                .replace('{MEMBER_COUNT}', guild.memberCount.toLocaleString())
+                                .replace(
+                                    '{MEMBER_CACHE_BEFORE_COUNT}',
+                                    beforeCacheSize.toLocaleString()
+                                )
+                                .replace(
+                                    '{MEMBER_CACHE_AFTER_COUNT}',
+                                    guild.members.cache.size.toLocaleString()
+                                ),
+                            error
+                        );
+                    }
                 }
 
                 // Get a list of memberIds
