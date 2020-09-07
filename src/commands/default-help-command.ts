94aaa1fa18c62c87c8937de4035c49b4170fda31
Kevin Novak <kevinmnovak@yahoo.com>
2020-09-07 00:33:53 -0400

    Refactor help command

--- src/commands/default-help-command.ts
+++ src/commands/default-help-command.ts
@@ -1,7 +1,7 @@
 import { DMChannel, Message, MessageEmbed, TextChannel } from 'discord.js';
 
-import { Command } from './command';
 import { MessageUtils } from '../utils';
+import { Command } from './command';
 
 let Config = require('../../config/config.json'); // Possible support for server specific prefixes?
 
@@ -15,113 +15,36 @@ export class DefaultHelpCommand implements Command {
     public voteOnly = false;
 
     public async execute(args: string[], msg: Message, channel: TextChannel | DMChannel) {
-        let prefix = Config.prefix;
         let embed = new MessageEmbed();
+        let clientAvatarUrl = msg.client.user.avatarURL();
 
-        if (args.length <= 2) {
+        let option = args[2]?.toLowerCase();
+        if (!option) {
             embed
-                .setAuthor('Birthday Bot General Help', msg.client.user.avatarURL())
-                .setDescription(
-                    `Birthday Bot celebrates user's birthdays and allows for a wide variety of settings and customizability for server owners. ` +
-                        '\n' +
-                        `\n**${prefix} set** - Set your birthday.` +
-                        `\n**${prefix} view [user]** - View your birthday or a users birthday.` +
-                        `\n**${prefix} next**\*\* - View next birthday(s) in the server.` +
-                        `\n**${prefix} list [page/date]**\*\* - View the server birthday list.` +
-                        `\n**${prefix} map** - View the time zone map.` +
-                        `\n**${prefix} invite** - Invite Birthday Bot to a server.` +
-                        `\n**${prefix} support** - Join the support server.` +
-                        `\n**${prefix} docs** - View the documentation.` +
-                        `\n**${prefix} faq** - View the frequently asked questions.` +
-                        `\n**${prefix} purge** - Remove your birthday data.` +
-                        `\n**${prefix} help setup** - Help for server setup.` +
-                        `\n**${prefix} help message** - Help for the birthday message settings.` +
-                        `\n**${prefix} help trusted** - Help for the trusted system.` +
-                        `\n**${prefix} settings**\*\* - View server's settings.` +
-                        `\n**${prefix} test [User]**\*\* - Test the birthday event.` +
-                        `\n` +
-                        `\n**${prefix} donate** - Support developments by donating!` +
-                        `\n` +
-                        `\nIf you have any question/problems please join our support server [here](${Config.links.support}).`
-                )
-                .addField('Legend', '\*\* = Server Only Command')
+                .setAuthor(HELP_GENERAL_TITLE, clientAvatarUrl)
+                .setDescription(HELP_GENERAL_DESC)
+                .addField('Legend', HELP_GENERAL_LEGEND)
                 .setColor(Config.colors.default);
-        } else if (args[2].toLowerCase() === 'setup') {
+        } else if (option === 'setup') {
             embed
-                .setAuthor('Birthday Bot Setup Help - Guild Only', msg.client.user.avatarURL())
-                .setDescription(
-                    '' +
-                        '\n' +
-                        `\n**${prefix} setup** - Interactive guide for server setup.` +
-                        '\n' +
-                        `\n**${prefix} create <channel/role>** - Create the default birthday role/channel.` +
-                        `\n**${prefix} update <channel/role> <#channel/@role>** - Update the birthday role/channel.` +
-                        `\n**${prefix} clear <channel/role>** - Clear the birthday role/channel.`
-                )
+                .setAuthor(HELP_SETUP_TITLE, clientAvatarUrl)
+                .setDescription(HELP_SETUP_DESC)
                 .setColor(Config.colors.default);
-        } else if (args[2].toLowerCase() === 'message') {
+        } else if (option === 'message') {
             embed
-                .setAuthor('Birthday Bot Message Help - Guild Only', msg.client.user.avatarURL())
-                .setDescription(
-                    '' +
-                        '\n' +
-                        `\n**${prefix} setup message** - Interactive guide for message settings setup.` +
-                        '\n' +
-                        `\n**${prefix} message list [page]** - List all custom birthday messages.` +
-                        `\n**${prefix} message add <message>** - Add a custom birthday message.\n - Placeholder for users: \`<Users>\`` +
-                        `\n**${prefix} message remove <position>** - Remove a certain birthday message.` +
-                        `\n**${prefix} message clear** - Clear all custom birthday messages.` +
-                        `\n**${prefix} message time <0-23>** - Set the birthday message time.` +
-                        `\n**${prefix} message mention <role/group>** - Set the birthday message mention setting.` +
-                        `\n**${prefix} message embed <T/F>** - Should the birthday message be embedded.` +
-                        `\n**${prefix} message test <position> [user count]** - Test a birthday message.`
-                )
+                .setAuthor(HELP_MESSAGE_TITLE, clientAvatarUrl)
+                .setDescription(HELP_MESSAGE_DESC)
                 .setColor(Config.colors.default);
-        } else if (args[2].toLowerCase() === 'trusted') {
+        } else if (option === 'trusted') {
             embed
-                .setAuthor(
-                    'Birthday Bot Trusted System Help - Guild Only',
-                    msg.client.user.avatarURL()
-                )
-                .setDescription(
-                    '' +
-                        '\n' +
-                        `\n**${prefix} setup trusted** - Interactive guide for trusted system settings setup.` +
-                        '\n' +
-                        `\n**${prefix} create trustedRole** - Create the default trusted role.` +
-                        `\n**${prefix} update trustedRole <channel>** - Update the trusted role.` +
-                        `\n**${prefix} clear trustedRole ** - Clear the trusted role.` +
-                        `\n**${prefix} trusted preventMsg <T/F>** - If trusted role is required for a birthday message.` +
-                        `\n**${prefix} trusted preventRole <T/F>** - If trusted role is required to get the birthday role.`
-                )
+                .setAuthor(HELP_TRUSTED_TITLE, clientAvatarUrl)
+                .setDescription(HELP_TRUSTED_DESC)
                 .setColor(Config.colors.default);
         } else {
             embed
-                .setAuthor('Birthday Bot General Help', msg.client.user.avatarURL())
-                .setDescription(
-                    `Birthday Bot celebrates user's birthdays and allows for a wide variety of settings and customizability for server owners. ` +
-                        '\n' +
-                        `\n**${prefix} set** - Set your birthday.` +
-                        `\n**${prefix} view [user]** - View your birthday or a users birthday.` +
-                        `\n**${prefix} next**\*\* - View next birthday(s) in the server.` +
-                        `\n**${prefix} list [page/date]**\*\* - View the server birthday list.` +
-                        `\n**${prefix} map** - View the time zone map.` +
-                        `\n**${prefix} invite** - Invite Birthday Bot to a server.` +
-                        `\n**${prefix} support** - Join the support server.` +
-                        `\n**${prefix} docs** - View the documentation.` +
-                        `\n**${prefix} faq** - View the frequently asked questions.` +
-                        `\n**${prefix} purge** - Remove your birthday data.` +
-                        `\n**${prefix} help setup** - Help for server setup.` +
-                        `\n**${prefix} help message** - Help for the birthday message settings.` +
-                        `\n**${prefix} help trusted** - Help for the trusted system.` +
-                        `\n**${prefix} settings**\*\* - View server's settings.` +
-                        `\n**${prefix} test [User]**\*\* - Test the birthday event.` +
-                        `\n` +
-                        `\n**${prefix} donate** - Support developments by donating!` +
-                        `\n` +
-                        `\nIf you have any question/problems please join our support server [here](${Config.links.support}).`
-                )
-                .addField('Legend', '\*\* = Server Only Command')
+                .setAuthor(HELP_GENERAL_TITLE, clientAvatarUrl)
+                .setDescription(HELP_GENERAL_DESC)
+                .addField('Legend', HELP_GENERAL_LEGEND)
                 .setColor(Config.colors.default);
         }
 
@@ -129,3 +52,59 @@ export class DefaultHelpCommand implements Command {
         else MessageUtils.sendDm(channel, embed);
     }
 }
+
+const HELP_GENERAL_TITLE = 'Birthday Bot General Help';
+const HELP_GENERAL_DESC =
+    `Birthday Bot celebrates user's birthdays and allows for a wide variety of settings and customizability for server owners. ` +
+    '\n' +
+    `\n**bday set** - Set your birthday.` +
+    `\n**bday view [user]** - View your birthday or a users birthday.` +
+    `\n**bday next**\*\* - View next birthday(s) in the server.` +
+    `\n**bday list [page/date]**\*\* - View the server birthday list.` +
+    `\n**bday map** - View the time zone map.` +
+    `\n**bday invite** - Invite Birthday Bot to a server.` +
+    `\n**bday support** - Join the support server.` +
+    `\n**bday docs** - View the documentation.` +
+    `\n**bday faq** - View the frequently asked questions.` +
+    `\n**bday purge** - Remove your birthday data.` +
+    `\n**bday help setup** - Help for server setup.` +
+    `\n**bday help message** - Help for the birthday message settings.` +
+    `\n**bday help trusted** - Help for the trusted system.` +
+    `\n**bday settings**\*\* - View server's settings.` +
+    `\n**bday test [User]**\*\* - Test the birthday event.` +
+    `\n` +
+    `\n**bday donate** - Support developments by donating!` +
+    `\n` +
+    `\nIf you have any question/problems please join our support server [here](${Config.links.support}).`;
+const HELP_GENERAL_LEGEND = '** = Server Only Command';
+
+const HELP_SETUP_TITLE = 'Birthday Bot Setup Help - Guild Only';
+const HELP_SETUP_DESC =
+    `\n**bday setup** - Interactive guide for server setup.` +
+    '\n' +
+    `\n**bday create <channel/role>** - Create the default birthday role/channel.` +
+    `\n**bday update <channel/role> <#channel/@role>** - Update the birthday role/channel.` +
+    `\n**bday clear <channel/role>** - Clear the birthday role/channel.`;
+
+const HELP_MESSAGE_TITLE = 'Birthday Bot Message Help - Guild Only';
+const HELP_MESSAGE_DESC =
+    `\n**bday setup message** - Interactive guide for message settings setup.` +
+    '\n' +
+    `\n**bday message list [page]** - List all custom birthday messages.` +
+    `\n**bday message add <message>** - Add a custom birthday message.\n - Placeholder for users: \`<Users>\`` +
+    `\n**bday message remove <position>** - Remove a certain birthday message.` +
+    `\n**bday message clear** - Clear all custom birthday messages.` +
+    `\n**bday message time <0-23>** - Set the birthday message time.` +
+    `\n**bday message mention <role/group>** - Set the birthday message mention setting.` +
+    `\n**bday message embed <T/F>** - Should the birthday message be embedded.` +
+    `\n**bday message test <position> [user count]** - Test a birthday message.`;
+
+const HELP_TRUSTED_TITLE = 'Birthday Bot Trusted System Help - Guild Only';
+const HELP_TRUSTED_DESC =
+    `\n**bday setup trusted** - Interactive guide for trusted system settings setup.` +
+    '\n' +
+    `\n**bday create trustedRole** - Create the default trusted role.` +
+    `\n**bday update trustedRole <channel>** - Update the trusted role.` +
+    `\n**bday clear trustedRole ** - Clear the trusted role.` +
+    `\n**bday trusted preventMsg <T/F>** - If trusted role is required for a birthday message.` +
+    `\n**bday trusted preventRole <T/F>** - If trusted role is required to get the birthday role.`;
