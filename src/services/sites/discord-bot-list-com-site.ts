22fd507b2844db97faf39e6699b71f365c80e62e
Kevin Novak <kevinmichaelnovak@gmail.com>
2020-09-11 01:24:20 -0400

    Updates from template (#83)
    * Update http service
    
    * Increase spawn timeout

--- src/services/sites/discord-bot-list-com-site.ts
+++ src/services/sites/discord-bot-list-com-site.ts
@@ -1,4 +1,4 @@
-import { BotSiteConfig } from '../../models/bot-site-config';
+import { BotSiteConfig } from '../../models/config-models';
 import { HttpService } from '../http-service';
 import { BotSite } from './bot-site';
 
@@ -11,6 +11,12 @@ export class DiscordBotListComSite implements BotSite {
     }
 
     public async updateServerCount(serverCount: number): Promise<void> {
-        await this.httpService.post(this.config.url, { guilds: serverCount }, this.config.token);
+        let res = await this.httpService.post(this.config.url, this.config.token, {
+            guilds: serverCount,
+        });
+
+        if (!res.ok) {
+            throw res;
+        }
     }
 }
