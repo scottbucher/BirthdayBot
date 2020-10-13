671ba1fe877c2f0069877eb1b327d81487f4967e
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-10-12 21:33:20 -0400

    Added the blacklist command handler

---
+++ src/commands/blacklist-command.ts
@@ -0,0 +1,46 @@
+import { Message, MessageEmbed, TextChannel } from 'discord.js';
+
+import { BlacklistAddSubCommand } from './blacklist/blacklist-add-sub-command';
+import { BlacklistClearSubCommand } from './blacklist/blacklist-clear-sub-command';
+import { BlacklistRemoveSubCommand } from './blacklist/blacklist-remove-sub-command';
+import { Command } from './command';
+
+let Config = require('../../config/config.json');
+
+export class MessageCommand implements Command {
+    public name: string = 'message';
+    public aliases = ['msg'];
+    public requireSetup = true;
+    public guildOnly = true;
+    public adminOnly = true;
+    public ownerOnly = false;
+    public voteOnly = false;
+
+    constructor(
+        private blacklistAddSubCommand: BlacklistAddSubCommand,
+        private blacklistRemoveSubCommand: BlacklistRemoveSubCommand,
+        private blacklistClearSubCommand: BlacklistClearSubCommand
+    ) {}
+
+    public async execute(args: string[], msg: Message, channel: TextChannel) {
+        if (args.length === 2) {
+            let embed = new MessageEmbed()
+                .setTitle('Invalid Usage!')
+                .setDescription(
+                    `Please specify a sub command for the blacklist! [(?)](${Config.links.docs}/faq#what-is-the-birthday-blacklist)\nAccepted Values: \`list\`, \`add <User>\`, \`remove <User>\`, \`clear\``
+                )
+                .setColor(Config.colors.error);
+            await channel.send(embed);
+            return;
+        }
+        if (args[2].toLowerCase() === 'list') {
+            // this.messageListSubCommand.execute(args, msg, channel);
+        } else if (args[2].toLowerCase() === 'clear') {
+            this.blacklistClearSubCommand.execute(args, msg, channel);
+        } else if (args[2].toLowerCase() === 'add') {
+            this.blacklistAddSubCommand.execute(args, msg, channel);
+        } else if (args[2].toLowerCase() === 'remove') {
+            this.blacklistRemoveSubCommand.execute(args, msg, channel);
+        }
+    }
+}
