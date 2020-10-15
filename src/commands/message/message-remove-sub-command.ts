446c0293d8f1701eef495529a86cc0340b9ce49f
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-10-15 19:04:36 -0400

    Allow removing a message by specifing a user

--- src/commands/message/message-remove-sub-command.ts
+++ src/commands/message/message-remove-sub-command.ts
@@ -1,6 +1,7 @@
 import { Message, MessageEmbed, TextChannel } from 'discord.js';
 
 import { CustomMessageRepo } from '../../services/database/repos';
+import { GuildUtils } from '../../utils';
 
 let Config = require('../../../config/config.json');
 
@@ -17,17 +18,45 @@ export class MessageRemoveSubCommand {
             await channel.send(embed);
             return;
         }
-        // Try and get the time
-        let position: number;
-        try {
-            position = parseInt(args[3]);
-        } catch (error) {
+
+        // Retrieve message to remove
+        let customMessages = await this.customMessageRepo.getCustomMessages(msg.guild.id);
+
+        if (!customMessages) {
             let embed = new MessageEmbed()
-                .setTitle('Invalid time!')
-                .setDescription('Accepted Values: `0-23`')
+                .setDescription('This server doesn\'t have any custom messages!')
                 .setColor(Config.colors.error);
             await channel.send(embed);
             return;
+        }
+
+        // Try and get the position
+        let position: number;
+
+        // Try and find someone they are mentioning
+        let target =
+            msg.mentions.members.first()?.user || GuildUtils.findMember(msg.guild, args[3])?.user;
+
+        // Did we find a user?
+        if (target) {
+            let userMessage = customMessages.customMessages.filter(
+                message => message.UserDiscordId === target.id
+            );
+
+            if (userMessage) position = userMessage[0].Position;
+        }
+
+        if (!position) {
+            try {
+                position = parseInt(args[3]);
+            } catch (error) {
+                let embed = new MessageEmbed()
+                    .setTitle('Invalid position!')
+                    .setDescription('Use `bday message list` to view your server\'s custom messages!')
+                    .setColor(Config.colors.error);
+                await channel.send(embed);
+                return;
+            }
         }
 
         if (!position) {
@@ -42,9 +71,7 @@ export class MessageRemoveSubCommand {
             return;
         }
 
-        // Retrieve message to remove
-        let messages = await this.customMessageRepo.getCustomMessages(msg.guild.id);
-        let message = messages.customMessages.find(question => question.Position === position);
+        let message = customMessages.customMessages.find(question => question.Position === position);
 
         if (!message) {
             let embed = new MessageEmbed()
