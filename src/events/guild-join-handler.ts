b2f4425557dd7f50eedf225abde7d9aba884578d
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-10-23 17:40:10 -0400

    Updated first join prompt

--- src/events/guild-join-handler.ts
+++ src/events/guild-join-handler.ts
@@ -1,4 +1,4 @@
-import { Guild, MessageEmbed } from 'discord.js';
+import { Guild, MessageEmbed, Permissions } from 'discord.js';
 
 import { EventHandler } from './event-handler';
 import { Logger } from '../services';
@@ -20,22 +20,26 @@ export class GuildJoinHandler implements EventHandler {
             .setAuthor(guild.name, guild.iconURL())
             .setTitle('Thank you for using Birthday Bot!')
             .setDescription(
-                `To view the commands of this bot use \`${prefix} help\`` +
-                    `\nTo setup the bot run \`${prefix} setup\`` +
-                    `\n\nView the [Documentation](${Config.links.docs}) or the [FAQ](${Config.links.docs}/faq).` +
+                `To support the bot and unlock special features use \`${prefix} premium\` in your server.` +
+                `\n\nTo view the commands of this bot use \`${prefix} help\`.` +
+                    `\nTo setup the bot run \`${prefix} setup\`.` +
+                    `\nTo set your birthday use \`${prefix} set\`.` +
+                    `\n\nView the [Documentation](${Config.links.docs}) or the [FAQ](${Config.links.docs}/faq.).` +
                     `\nFor more support join our discord server [here](${Config.links.support})!`
             )
             .setFooter('Join our support server for help!', guild.iconURL())
             .setTimestamp()
             .setColor(Config.colors.default);
 
-        // Get the guild owner
-        let owner = guild.owner;
-        if (!owner) {
-            return;
+        // Get someone to message
+        let user = guild.owner;
+        if (!user) {
+            user = guild.members.cache.find(member => member.hasPermission(Permissions.FLAGS.ADMINISTRATOR));
         }
 
-        let ownerChannel = await guild.owner.createDM();
-        await MessageUtils.send(ownerChannel, embed);
+        if (!user) return;
+
+        let userChannel = await user.createDM();
+        await MessageUtils.send(userChannel, embed);
     }
 }
