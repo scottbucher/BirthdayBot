2b7ab8530b8df895b645dda98a0ecddf3093cb7c
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-12-09 16:26:30 -0500

    Improved error checking

--- src/utils/action-utils.ts
+++ src/utils/action-utils.ts
@@ -1,11 +1,19 @@
-import { GuildMember, Message, Role } from 'discord.js';
+import { DiscordAPIError, GuildMember, Message, Role } from 'discord.js';
 
 export abstract class ActionUtils {
     public static async giveRole(member: GuildMember, role: Role): Promise<void> {
         try {
             await member.roles.add(role);
         } catch (error) {
-            // Can't give that role
+            // Error code 50001: "Missing Access", Error code: 10011: "Unknown Role" (Role was deleted), Error code: 50013: "Missing Permission"
+            if (
+                error instanceof DiscordAPIError &&
+                (error.code === 50001 || error.code === 10011 || error.code === 50013)
+            ) {
+                return;
+            } else {
+                throw error;
+            }
         }
     }
 
@@ -13,7 +21,15 @@ export abstract class ActionUtils {
         try {
             await member.roles.remove(role);
         } catch (error) {
-            // Can't take that role
+            // Error code 50001: "Missing Access", Error code: 10011: "Unknown Role" (Role was deleted), Error code: 50013: "Missing Permission"
+            if (
+                error instanceof DiscordAPIError &&
+                (error.code === 50001 || error.code === 10011 || error.code === 50013)
+            ) {
+                return;
+            } else {
+                throw error;
+            }
         }
     }
 }
