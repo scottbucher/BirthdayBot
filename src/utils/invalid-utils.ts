12d342465fa4cd6c956f2651b70b36d71f891e9b
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-12-11 01:52:13 -0500

    New util for responding to invalid command usage

---
+++ src/utils/invalid-utils.ts
@@ -0,0 +1,78 @@
+import {
+    DMChannel,
+    Message,
+    MessageEmbed,
+    Role,
+    StringResolvable,
+    TextChannel,
+    User,
+} from 'discord.js';
+
+import { MessageUtils } from './message-utils';
+
+let Config = require('../../config/config.json');
+
+export abstract class InvalidUtils {
+    public static async notEnoughPermissions(
+        target: User | DMChannel | TextChannel,
+        permissions: String[]
+    ): Promise<void> {
+        let permList = permissions.map(perm => `\`${perm}\``).join('\n');
+        let embed = new MessageEmbed()
+            .setTitle('Not Enough Permissions!')
+            .setColor(Config.colors.error)
+            .setDescription(
+                'Birthday Bot does not have enough permission to perform this action.' +
+                    `\n\nNeeded Permission${
+                        permissions.length > 1 ? 's' : ''
+                    } (In the server and channel!):` +
+                    `\n${permList}`
+            )
+            .setFooter(
+                'Please join our support server if you have any questions.',
+                target.client.user.avatarURL()
+            );
+        await MessageUtils.send(target, embed);
+    }
+
+    public static async cantSendInChannel(
+        target: User | DMChannel | TextChannel,
+        issueChannel: User | DMChannel | TextChannel,
+        permissions: String[]
+    ): Promise<void> {
+        let permList = permissions.map(perm => `\`${perm}\``).join('\n');
+        let embed = new MessageEmbed()
+            .setTitle('Not Enough Permissions!')
+            .setColor(Config.colors.error)
+            .setDescription(
+                `Birthday Bot does not have enough permission in ${issueChannel.toString()} to preform this action.` +
+                    `\n\nNeeded Permission${permissions.length > 1 ? 's' : ''}:` +
+                    `\n${permList}`
+            )
+            .setFooter(
+                'Please join our support server if you have any questions.',
+                target.client.user.avatarURL()
+            );
+        await MessageUtils.send(target, embed);
+    }
+
+    public static async roleHierarchyError(
+        target: User | DMChannel | TextChannel,
+        role: Role
+    ): Promise<void> {
+        let embed = new MessageEmbed()
+            .setTitle('Role Hierarchy Error!')
+            .setColor(Config.colors.error)
+            .setDescription(
+                `The birthday role must be bellow ${target.client.user.toString()}'s role.` +
+                    `\nAdditional Note: ${target.client.user.toString()}'s role must be higher than the users it is assigning the birthday role to.` +
+                    `\n\nExample Role Hierarchy:\n\`Birthday Bot's Role\`\n\`Birthday Role\`\n\`Birthday User's Highest Role\`` +
+                    `\n\nEssentially the bot is unable to give a role to someone with a higher role than them or give a role that is higher than the bot's.`
+            )
+            .setFooter(
+                'Please join our support server if you have any questions.',
+                target.client.user.avatarURL()
+            );
+        await MessageUtils.send(target, embed);
+    }
+}
