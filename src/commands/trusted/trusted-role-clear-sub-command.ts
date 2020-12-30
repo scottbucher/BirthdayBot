aef860d32d6e2415dffb74d03f33b27f5519cd3e
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-12-30 01:30:26 -0500

    Added command to clear all trusted roles

---
+++ src/commands/trusted/trusted-role-clear-sub-command.ts
@@ -0,0 +1,99 @@
+import {
+    CollectOptions,
+    CollectorUtils,
+    ExpireFunction,
+    MessageFilter,
+} from 'discord.js-collector-utils';
+import { Message, MessageEmbed, MessageReaction, Role, TextChannel, User } from 'discord.js';
+
+import { MessageUtils } from '../../utils';
+import { TrustedRoleRepo } from '../../services/database/repos/trusted-role-repo';
+
+let Config = require('../../../config/config.json');
+
+const COLLECT_OPTIONS: CollectOptions = {
+    time: Config.experience.promptExpireTime * 1000,
+    reset: true,
+};
+
+export class TrustedRoleClearSubCommand {
+    constructor(private trustedRoleRepo: TrustedRoleRepo) {}
+
+    public async execute(args: string[], msg: Message, channel: TextChannel) {
+        let stopFilter: MessageFilter = (nextMsg: Message) =>
+            nextMsg.author.id === msg.author.id &&
+            [Config.prefix, ...Config.stopCommands].includes(
+                nextMsg.content.split(/\s+/)[0].toLowerCase()
+            );
+        let expireFunction: ExpireFunction = async () => {
+            await MessageUtils.send(
+                channel,
+                new MessageEmbed()
+                    .setTitle('Trusted Role Clear - Expired')
+                    .setDescription('Type `bday trustedRole clear` to clear the trusted roles.')
+                    .setColor(Config.colors.error)
+            );
+        };
+
+        let trustedRoles = await this.trustedRoleRepo.getTrustedRoles(msg.guild.id);
+
+        let confirmationEmbed = new MessageEmbed();
+
+        if (trustedRoles.trustedRoles.length === 0) {
+            confirmationEmbed
+                .setDescription(`You server has not set any trusted roles set!`)
+                .setColor(Config.colors.error);
+            await MessageUtils.send(channel, confirmationEmbed);
+            return;
+        }
+
+        let trueFalseOptions = [Config.emotes.confirm, Config.emotes.deny];
+
+        confirmationEmbed
+            .setDescription(
+                `Are you sure you want to clear __**${
+                    trustedRoles.trustedRoles.length
+                }**__ trusted role${trustedRoles.trustedRoles.length === 1 ? '' : 's'}?`
+            )
+            .setFooter('This action is irreversible!', msg.client.user.avatarURL())
+            .setColor(Config.colors.warning);
+
+        let confirmationMessage = await MessageUtils.send(channel, confirmationEmbed); // Send confirmation and emotes
+        for (let option of trueFalseOptions) {
+            await MessageUtils.react(confirmationMessage, option);
+        }
+
+        let confirmation: string = await CollectorUtils.collectByReaction(
+            confirmationMessage,
+            // Collect Filter
+            (msgReaction: MessageReaction, reactor: User) =>
+                reactor.id === msg.author.id && trueFalseOptions.includes(msgReaction.emoji.name),
+            stopFilter,
+            // Retrieve Result
+            async (msgReaction: MessageReaction, reactor: User) => {
+                return msgReaction.emoji.name;
+            },
+            expireFunction,
+            COLLECT_OPTIONS
+        );
+
+        MessageUtils.delete(confirmationMessage);
+
+        if (confirmation === undefined) return;
+
+        if (confirmation === Config.emotes.confirm) {
+            // Confirm
+            await this.trustedRoleRepo.clearTrustedRoles(msg.guild.id);
+
+            let embed = new MessageEmbed()
+                .setDescription(`Successfully cleared all trusted roles from the database!`)
+                .setColor(Config.colors.success);
+            await MessageUtils.send(channel, embed);
+        } else {
+            let embed = new MessageEmbed()
+                .setDescription(`Action canceled.`)
+                .setColor(Config.colors.success);
+            await MessageUtils.send(channel, embed);
+        }
+    }
+}
