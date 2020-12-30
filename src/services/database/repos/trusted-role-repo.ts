80ae817c8fb2ddb904474301baf2ffc2df3ca936
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-12-29 22:11:01 -0500

    Use correct procedure and variable names

--- src/services/database/repos/trusted-role-repo.ts
+++ src/services/database/repos/trusted-role-repo.ts
@@ -7,34 +7,22 @@ import { TrustedRoles } from '../../../models/database/trusted-role-models';
 export class TrustedRoleRepo {
     constructor(private dataAccess: DataAccess) {}
 
-    public async addTrustedRole(discordId: string): Promise<void> {
-        await this.dataAccess.executeProcedure(Procedure.TrustedRole_Add, [discordId]);
+    public async addTrustedRole(discordId: string, roleId: string): Promise<void> {
+        await this.dataAccess.executeProcedure(Procedure.TrustedRole_Add, [discordId, roleId]);
     }
 
-    public async removeCustomMessage(
-        discordId: string,
-        value: number,
-        type: string
-    ): Promise<void> {
-        await this.dataAccess.executeProcedure(Procedure.Message_Remove, [discordId, value, type]);
+    public async removeTrustedRole(discordId: string, roleId: string): Promise<void> {
+        await this.dataAccess.executeProcedure(Procedure.TrustedRole_Remove, [discordId, roleId]);
     }
 
-    public async removeTrustedRole(discordId: string, value: number, type: string): Promise<void> {
-        await this.dataAccess.executeProcedure(Procedure.Message_RemoveUser, [
+    public async clearTrustedRoles(discordId: string): Promise<void> {
+        await this.dataAccess.executeProcedure(Procedure.TrustedRole_Clear, [discordId]);
+    }
+
+    public async getTrustedRoles(discordId: string, roleId: string): Promise<TrustedRoles> {
+        let results = await this.dataAccess.executeProcedure(Procedure.TrustedRole_Get, [
             discordId,
-            value,
-            type,
-        ]);
-    }
-
-    public async clearTrustedRoles(discordId: string, type: string): Promise<void> {
-        await this.dataAccess.executeProcedure(Procedure.Message_Clear, [discordId, type]);
-    }
-
-    public async getTrustedRoles(discordId: string, type: string): Promise<TrustedRoles> {
-        let results = await this.dataAccess.executeProcedure(Procedure.Message_Get, [
-            discordId,
-            type,
+            roleId,
         ]);
 
         let trustedRoles = SQLUtils.getTable(results, 0);
@@ -47,7 +35,7 @@ export class TrustedRoleRepo {
         page: number,
         type: string
     ): Promise<TrustedRoles> {
-        let results = await this.dataAccess.executeProcedure(Procedure.Message_GetList, [
+        let results = await this.dataAccess.executeProcedure(Procedure.TrustedRole_GetList, [
             guildId,
             pageSize,
             page,
