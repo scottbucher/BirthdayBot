f16ddbd8b96cbae0d1a3412285dd4cc51467b43f
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2020-12-30 01:30:01 -0500

    Added list for trusted role

---
+++ src/commands/trusted/trusted-role-list-sub-command.ts
@@ -0,0 +1,49 @@
+import { FormatUtils, MessageUtils, ParseUtils } from '../../utils';
+import { Message, TextChannel } from 'discord.js';
+
+import { TrustedRoleRepo } from '../../services/database/repos/trusted-role-repo';
+
+let Config = require('../../../config/config.json');
+
+export class TrustedRoleListSubCommand {
+    constructor(private trustedRoleRepo: TrustedRoleRepo) {}
+
+    public async execute(args: string[], msg: Message, channel: TextChannel, hasPremium: boolean) {
+        let page = 1;
+
+        if (args[3]) {
+            try {
+                page = ParseUtils.parseInt(args[4]);
+            } catch (error) {
+                // Not A Number
+            }
+            if (!page || page <= 0 || page > 100000) page = 1;
+        }
+
+        let pageSize = Config.experience.birthdayMessageListSize;
+
+        let trustedRoleResults = await this.trustedRoleRepo.getTrustedRoleList(
+            msg.guild.id,
+            pageSize,
+            page
+        );
+
+        if (page > trustedRoleResults.stats.TotalPages) page = trustedRoleResults.stats.TotalPages;
+
+        let embed = await FormatUtils.getTrustedRoleList(
+            msg.guild,
+            trustedRoleResults,
+            page,
+            pageSize,
+            hasPremium
+        );
+
+        let message = await MessageUtils.send(channel, embed);
+
+        if (embed.description.includes('**No Trusted')) return;
+
+        await MessageUtils.react(message, Config.emotes.previousPage);
+        await MessageUtils.react(message, Config.emotes.jumpToPage);
+        await MessageUtils.react(message, Config.emotes.nextPage);
+    }
+}
