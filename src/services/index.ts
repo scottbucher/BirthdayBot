aeacab8cd0ab76a495309ec80ce64afd97020b97
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-01-06 02:17:41 -0500

    Implemented lang system

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export { HttpService } from './http-service';
 export { Logger } from './logger';
 export { DiscordService } from './discord-service';
 export { SubscriptionService } from './subscription-service';
+export { Lang } from './lang';
---
+++ src/services/lang.ts
@@ -0,0 +1,26 @@
+import { LangCode } from '../models/enums';
+import { MessageEmbed } from 'discord.js';
+import { MultilingualService } from 'discord.js-multilingual-utils';
+import path from 'path';
+
+export class Lang {
+    private static multilingualService: MultilingualService = new MultilingualService(
+        path.resolve(__dirname, '../../lang')
+    );
+
+    public static getRef(
+        refName: string,
+        langCode: LangCode,
+        variables?: { [name: string]: string }
+    ): string {
+        return this.multilingualService.getRef(refName, langCode.toLowerCase(), variables);
+    }
+
+    public static getEmbed(
+        embedName: string,
+        langCode: LangCode,
+        variables?: { [name: string]: string }
+    ): MessageEmbed {
+        return this.multilingualService.getEmbed(embedName, langCode.toLowerCase(), variables);
+    }
+}
