a73e7f8e8b352ce8d10c7edb6b809a2ce0c4736a
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-01-24 21:31:38 -0500

    Added model for anniversary roles

---
+++ src/models/database/member-anniversary-role-models.ts
@@ -0,0 +1,16 @@
+import { StatsData } from './stats-models';
+
+export class MemberAnniversaryRoles {
+    memberAnniversaryRoles: MemberAnniversaryRole[];
+    stats: StatsData;
+
+    constructor(memberAnniversaryRoleRows: MemberAnniversaryRole[], statsRow: StatsData) {
+        this.memberAnniversaryRoles = memberAnniversaryRoleRows;
+        this.stats = statsRow;
+    }
+}
+
+export class MemberAnniversaryRole {
+    MemberAnniversaryRoleDiscordId: string;
+    Position: number;
+}
