ce92f8bea3ae0c29f4a903d4006f75ae7fedb213
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-02-12 22:39:29 -0500

    READ_MESSAGE_HISTORY needed to ADD_REACTIONS

--- src/utils/permission-utils.ts
+++ src/utils/permission-utils.ts
@@ -4,25 +4,33 @@ import { Command } from '../commands';
 import { GuildData } from '../models/database';
 
 export class PermissionUtils {
-    public static canSend(channel: TextChannel | DMChannel | NewsChannel): boolean {
-        if (channel instanceof DMChannel) return true;
+    public static canSend(channel: DMChannel | TextChannel | NewsChannel): boolean {
+        // Bot always has permission in direct message
+        if (channel instanceof DMChannel) {
+            return true;
+        }
 
-        let channelPerms = channel?.permissionsFor(channel.client.user);
+        let channelPerms = channel.permissionsFor(channel.client.user);
         if (!channelPerms) {
             // This can happen if the guild disconnected while a collector is running
             return false;
         }
+
         // VIEW_CHANNEL - Needed to view the channel
         // SEND_MESSAGES - Needed to send messages
         // EMBED_LINKS - Needed to send embedded links
-        // ADD_REACTIONS - Needed to add reactions
+        // ADD_REACTIONS - Needed to add new reactions to messages
+        // READ_MESSAGE_HISTORY - Needed to add new reactions to messages
+        //    https://discordjs.guide/popular-topics/permissions-extended.html#implicit-permissions
         return channelPerms.has([
             Permissions.FLAGS.VIEW_CHANNEL,
             Permissions.FLAGS.SEND_MESSAGES,
             Permissions.FLAGS.EMBED_LINKS,
             Permissions.FLAGS.ADD_REACTIONS,
+            Permissions.FLAGS.READ_MESSAGE_HISTORY,
         ]);
     }
+
     public static canReact(
         channel: DMChannel | TextChannel | NewsChannel,
         removeOthers: boolean = false
@@ -52,9 +60,12 @@ export class PermissionUtils {
     }
 
     public static canHandleReaction(channel: TextChannel | DMChannel | NewsChannel): boolean {
-        if (channel instanceof DMChannel) return true;
+        // Bot always has permission in direct message
+        if (channel instanceof DMChannel) {
+            return true;
+        }
 
-        let channelPerms = channel?.permissionsFor(channel.client.user);
+        let channelPerms = channel.permissionsFor(channel.client.user);
         if (!channelPerms) {
             // This can happen if the guild disconnected while a collector is running
             return false;
