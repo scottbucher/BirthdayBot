a8776997bdae603fcdd9e0bb7cac04fe56d9024f
Stqlth <stqlth@gmail.com>
2021-04-03 21:36:11 -0400

    Updated utils

--- src/utils/math-utils.ts
+++ src/utils/math-utils.ts
@@ -1,4 +1,7 @@
 export abstract class MathUtils {
+    public static sum(numbers: number[]): number {
+        return numbers.reduce((a, b) => a + b, 0);
+    }
     public static clamp(input: number, min: number, max: number): number {
         return Math.min(Math.max(input, min), max);
     }
--- src/utils/shard-utils.ts
+++ src/utils/shard-utils.ts
@@ -1,11 +1,26 @@
 import { ShardClientUtil, ShardingManager, Util } from 'discord.js';
+import { MathUtils } from '.';
 
 export abstract class ShardUtils {
-    public static async getRecommendedShards(
-        token: string,
-        serversPerShard: number
-    ): Promise<number> {
+    public static async recommendedShards(token: string, serversPerShard: number): Promise<number> {
         return Math.ceil(await Util.fetchRecommendedShards(token, serversPerShard));
+    }
+
+    public static shardIds(shardInterface: ShardingManager | ShardClientUtil): number[] {
+        if (shardInterface instanceof ShardingManager) {
+            return shardInterface.shards.map(shard => shard.id);
+        } else if (shardInterface instanceof ShardClientUtil) {
+            return shardInterface.ids;
+        }
+    }
+
+    public static async serverCount(
+        shardInterface: ShardingManager | ShardClientUtil
+    ): Promise<number> {
+        let shardGuildCounts: number[] = await shardInterface.fetchClientValues(
+            'guilds.cache.size'
+        );
+        return MathUtils.sum(shardGuildCounts);
     }
 
     public static getMyShardIds(
