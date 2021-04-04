474bc07e6f49531e4017fda369ea5d22c7e5d61e
Stqlth <stqlth@gmail.com>
2021-04-03 21:07:03 -0400

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
-import express, { ErrorRequestHandler, Express } from 'express';
+import express, { ErrorRequestHandler, Express, RequestHandler } from 'express';
+import util from 'util';
 
 import { Controller } from './controllers';
 import { Logger } from './services';
-import util from 'util';
 
 let Config = require('../config/config.json');
 let Logs = require('../lang/logs.json');
@@ -13,25 +13,31 @@ export class Api {
     constructor(public controllers: Controller[]) {
         this.app = express();
         this.app.use(express.json());
+        this.app.use(this.checkAuth);
+        this.app.use(this.handleError);
         this.setupControllers();
     }
 
-    // Start Api
     public async start(): Promise<void> {
         let listen = util.promisify(this.app.listen.bind(this.app));
         await listen(Config.api.port);
-        Logger.info(Logs.info.startedApi.replace('{PORT}', Config.api.port));
+        Logger.info(Logs.info.apiStarted.replace('{PORT}', Config.api.port));
     }
 
-    // Setup the controllers
     private setupControllers(): void {
         for (let controller of this.controllers) {
-            controller.router.use(this.handleError);
             this.app.use('/', controller.router);
         }
     }
 
-    // Handle errors from the controllers
+    private checkAuth: RequestHandler = (req, res, next) => {
+        if (req.headers.authorization !== Config.api.secret) {
+            res.sendStatus(401);
+            return;
+        }
+        next();
+    };
+
     private handleError: ErrorRequestHandler = (error, req, res, next) => {
         Logger.error(
             Logs.error.apiRequest.replace('{HTTP_METHOD}', req.method).replace('{URL}', req.url),
