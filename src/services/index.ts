389ae83e03f275217f028646ec3efd3122ff1ea4
Stqlth <stqlth@gmail.com>
2021-04-03 21:33:59 -0400

    Added mster api service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export { BirthdayService } from './birthday-service';
 export { HttpService } from './http-service';
 export { Logger } from './logger';
-export { DiscordService } from './discord-service';
-export { SubscriptionService } from './subscription-service';
+export { MasterApiService } from './master-api-services';
