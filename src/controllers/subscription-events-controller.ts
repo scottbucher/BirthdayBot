5a8cc69bc519a4a4717d1804bded1964fbad8f0a
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-04-19 10:36:48 -0400

    Changing controller to work on cluster level

--- src/controllers/subscription-events-controller.ts
+++ src/controllers/subscription-events-controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response, Router } from 'express';
+import { SubscriptionStatus, SubscriptionStatusName } from '../models/subscription-models';
 
 import { Controller } from './controller';
 import { ShardingManager } from 'discord.js';
-import { SubscriptionStatusName } from '../models/subscription-models';
+import { checkAuth } from '../middleware';
 import router from 'express-promise-router';
 
 let Config = require('../../config/config.json');
@@ -12,22 +13,20 @@ export class SubscriptionEventsController implements Controller {
     public router: Router = router();
 
     constructor(private shardManager: ShardingManager) {
+        this.router.use(checkAuth(Config.api.secret));
         this.router.post(this.path, (req, res) => this.post(req, res));
     }
 
     private async post(req: Request, res: Response): Promise<void> {
-        if (req.headers.authorization !== Config.payments.token) {
-            res.send(401);
-            return;
-        }
+        let resBody: SubscriptionStatus = res.locals.input;
 
-        let subscriber = req.body.subscriber;
-        let plan = req.body.plan;
-        let status = req.body.subscription?.status;
+        let subscriber = resBody.subscriber;
+        let plan = resBody.plan;
+        let status = resBody.subscription?.status;
 
         if (!(subscriber && plan && status)) res.sendStatus(400);
 
-        switch (status) {
+        switch (resBody.subscription?.status) {
             case SubscriptionStatusName.ACTIVE:
             case SubscriptionStatusName.CANCELLED:
             case SubscriptionStatusName.EXPIRED: {
