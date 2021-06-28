b62818bfcdd9d29eb774e2e1db378aa421beaaab
Stqlth <stqlth@gmail.com>
2021-06-28 09:22:32 -0400

    Added claim command

--- src/commands/memberAnniversaryRole/index.ts
+++ src/commands/memberAnniversaryRole/index.ts
@@ -2,3 +2,4 @@ export { MemberAnniversaryRoleAddSubCommand } from './member-anniversary-role-ad
 export { MemberAnniversaryRoleRemoveSubCommand } from './member-anniversary-role-remove-sub-command';
 export { MemberAnniversaryRoleClearSubCommand } from './member-anniversary-role-clear-sub-command';
 export { MemberAnniversaryRoleListSubCommand } from './member-anniversary-role-list-sub-command';
+export { MemberAnniversaryRoleClaimSubCommand } from './member-anniversary-role-claim-command'
