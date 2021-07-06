289d1b122692539d010622748947f8960e837464
Stqlth <stqlth@gmail.com>
2021-07-06 14:22:59 -0400

    Add config value for delays

--- src/utils/action-utils.ts
+++ src/utils/action-utils.ts
@@ -1,9 +1,12 @@
 import { DiscordAPIError, GuildMember, Role } from 'discord.js';
 import { TimeUtils } from './time-utils';
 
+let Config = require('../../config/config.json');
+
 export class ActionUtils {
     // TODO: Have giveRole and removeRole take in an interval to sleep for to prevent rate limits
     public static async giveRole(member: GuildMember, role: Role, delay?: number): Promise<void> {
+        delay = Config.delays.enabled ? delay : 0;
         try {
             await member.roles.add(role);
             await TimeUtils.sleep(delay ?? 0);
@@ -20,6 +23,7 @@ export class ActionUtils {
     }
 
     public static async removeRole(member: GuildMember, role: Role, delay?: number): Promise<void> {
+        delay = Config.delays.enabled ? delay : 0;
         try {
             await member.roles.remove(role);
             await TimeUtils.sleep(delay ?? 0);
