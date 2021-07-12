b24954e097e939f4b7221f9c9697118f51ecf4e2
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-07-12 19:59:59 -0400

    Remove unneeded log (undefined log)

--- src/bot.ts
+++ src/bot.ts
@@ -65,7 +65,6 @@ export class Bot {
 
         if (!Debug.dummyMode.enabled) {
             this.jobService.start();
-            Logger.info(Logs.info.startedJobs);
         }
 
         this.ready = true;
