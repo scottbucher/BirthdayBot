69d121d3e6624eb107f96bf4995a4f0f4f5ff98c
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-09-16 17:10:42 -0400

    Added support for roles in blacklist and the ability to remove by id

--- src/commands/blacklist/blacklist-remove-sub-command.ts
+++ src/commands/blacklist/blacklist-remove-sub-command.ts
@@ -1,5 +1,5 @@
 import { GuildUtils, MessageUtils } from '../../utils';
-import { Message, TextChannel } from 'discord.js';
+import { Message, Role, TextChannel } from 'discord.js';
 
 import { BlacklistRepo } from '../../services/database/repos';
 import { Lang } from '../../services';
@@ -12,7 +12,7 @@ export class BlacklistRemoveSubCommand {
         if (args.length === 3) {
             await MessageUtils.send(
                 channel,
-                Lang.getEmbed('validation.noUserSpecified', LangCode.EN_US)
+                Lang.getEmbed('validation.noUserOrRoleSpecified', LangCode.EN_US)
             );
             return;
         }
@@ -21,34 +21,54 @@ export class BlacklistRemoveSubCommand {
         let target =
             msg.mentions.members.first()?.user || GuildUtils.findMember(msg.guild, args[3])?.user;
 
-        // Did we find a user?
+        // Get the role they are mentioning
+        let role: Role = msg.mentions.roles.first();
+
+        let id: string;
+
         if (!target) {
-            await MessageUtils.send(
-                channel,
-                Lang.getEmbed('validation.noUserFound', LangCode.EN_US)
-            );
-            return;
+            if (!role) {
+                role = msg.guild.roles.cache.find(
+                    role =>
+                        role.name.toLowerCase().includes(args[3].toLowerCase()) ||
+                        role.id === args[3].toLowerCase()
+                );
+            }
+
+            if (role?.id === msg.guild.id || args[3].toLowerCase() === 'everyone') {
+                MessageUtils.send(channel, Lang.getEmbed('validation.invalidRole', LangCode.EN_US));
+                return;
+            }
+        } else {
+            if (target.bot) {
+                await MessageUtils.send(
+                    channel,
+                    Lang.getEmbed('validation.cantBlacklistBot', LangCode.EN_US)
+                );
+                return;
+            }
         }
 
-        if (target.bot) {
-            await MessageUtils.send(
-                channel,
-                Lang.getEmbed('validation.cantBlacklistBot', LangCode.EN_US)
-            );
-            return;
-        }
+        id = target ? target.id : role ? role.id : args[3];
 
         let blacklist = await this.blacklistRepo.getBlacklist(msg.guild.id);
 
-        if (!blacklist.blacklist.map(entry => entry.UserDiscordId).includes(target.id)) {
+        if (!blacklist.blacklist.map(entry => entry.UserDiscordId).includes(id)) {
+            // If we have a user or role use the correct type, otherwise default to combining them (user/role) in the message
             await MessageUtils.send(
                 channel,
-                Lang.getEmbed('validation.userNotInBlacklist', LangCode.EN_US)
+                !target && !role
+                    ? Lang.getRef('types.user', LangCode.EN_US) +
+                          '/' +
+                          Lang.getRef('types.role', LangCode.EN_US)
+                    : Lang.getEmbed('validation.userOrRoleNotInBlacklist', LangCode.EN_US, {
+                          TYPE: Lang.getRef('types.' + (target ? 'user' : 'role'), LangCode.EN_US),
+                      })
             );
             return;
         }
 
-        await this.blacklistRepo.removeBlacklist(msg.guild.id, target.id);
+        await this.blacklistRepo.removeBlacklist(msg.guild.id, target ? target.id : role.id);
 
         await MessageUtils.send(
             channel,
