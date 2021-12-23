3edc346a96fa0dcd39bf2b730baffdf41ecfc46f
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-12-23 16:29:53 -0500

    Moved blacklist command to match previous structure

---
+++ src/commands/blacklist-command.ts
@@ -0,0 +1,121 @@
+import { ApplicationCommandData, CommandInteraction } from 'discord.js';
+
+import { ApplicationCommandOptionType } from 'discord-api-types';
+import { Command } from './command';
+import { EventData } from '../models/internal-models';
+import { Lang } from '../services';
+import { MessageUtils } from '../utils';
+
+// did I not split this up correctly?
+export class BlacklistAddUserCommand implements Command {
+    public data: ApplicationCommandData = {
+        name: Lang.getCom('commands.blacklist'),
+        description: 'Manage the blacklist.',
+        options: [
+            {
+                name: Lang.getCom('subCommands.add'),
+                description: 'Add something to the blacklist',
+                type: ApplicationCommandOptionType.SubcommandGroup.valueOf(),
+                options: [
+                    {
+                        name: Lang.getCom('subCommands.user'),
+                        description: 'Add a user to the blacklist.',
+                        type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                        options: [
+                            {
+                                name: Lang.getCom('arguments.user'),
+                                description: 'User.',
+                                type: ApplicationCommandOptionType.User.valueOf(),
+                                required: true,
+                            },
+                        ],
+                    },
+                    {
+                        name: Lang.getCom('subCommands.role'),
+                        description: 'Add a role to the blacklist.',
+                        type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                        options: [
+                            {
+                                name: Lang.getCom('arguments.role'),
+                                description: 'Role.',
+                                type: ApplicationCommandOptionType.Role.valueOf(),
+                                required: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                name: Lang.getCom('subCommands.remove'),
+                description: 'Remove something from the blacklist.',
+                type: ApplicationCommandOptionType.SubcommandGroup.valueOf(),
+                options: [
+                    {
+                        name: Lang.getCom('subCommands.user'),
+                        description: 'Remove a user from the blacklist.',
+                        type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                        options: [
+                            {
+                                name: Lang.getCom('arguments.user'),
+                                description: 'User.',
+                                type: ApplicationCommandOptionType.User.valueOf(),
+                                required: true,
+                            },
+                        ],
+                    },
+                    {
+                        name: Lang.getCom('subCommands.role'),
+                        description: 'Remove a role from the blacklist.',
+                        type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                        options: [
+                            {
+                                name: Lang.getCom('arguments.role'),
+                                description: 'Role.',
+                                type: ApplicationCommandOptionType.Role.valueOf(),
+                                required: true,
+                            },
+                        ],
+                    },
+                    {
+                        name: Lang.getCom('subCommands.id'),
+                        description: 'Remove an ID from the blacklist.',
+                        type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                        options: [
+                            {
+                                name: Lang.getCom('arguments.id'),
+                                description: 'ID.',
+                                type: ApplicationCommandOptionType.String.valueOf(),
+                                required: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                name: Lang.getCom('subCommands.clear'),
+                description: 'Clear the blacklist.',
+                type: ApplicationCommandOptionType.Subcommand.valueOf(),
+            },
+            {
+                name: Lang.getCom('subCommands.list'),
+                description: 'Show the blacklist.',
+                type: ApplicationCommandOptionType.Subcommand.valueOf(),
+                options: [
+                    {
+                        name: Lang.getCom('arguments.page'),
+                        description: 'Page number.',
+                        type: ApplicationCommandOptionType.Number.valueOf(),
+                        required: false,
+                    },
+                ],
+            },
+        ],
+    };
+    public requireDev = false;
+    public requireGuild = false;
+    public requirePerms = [];
+
+    public async execute(intr: CommandInteraction, data: EventData): Promise<void> {
+        await MessageUtils.sendIntr(intr, Lang.getEmbed('displayEmbeds.help', data.lang()));
+    }
+}
