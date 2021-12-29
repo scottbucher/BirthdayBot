81cb1be44f9fbe20b606a75480e4cc6fd9ee5490
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2021-12-29 15:54:17 -0500

    indexed models

--- src/models/cluster-api/index.ts
+++ src/models/cluster-api/index.ts
@@ -1,2 +1,4 @@
 export { GetGuildsResponse } from './guilds';
 export { GetShardsResponse, ShardInfo, ShardStats, SetShardPresencesRequest } from './shards';
+export { Subscription, SendSubscriptionEventRequest } from './subscription-events';
+export { SendVoteRequest } from './votes';
--- src/models/enums/index.ts
+++ src/models/enums/index.ts
@@ -1,1 +1,2 @@
 export { LangCode, Language } from './language';
+export { DateFormatOption } from './date-format';
