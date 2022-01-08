3a4dc86e597ea45d9eb22065f5d5b9bde3597be4
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-01-07 21:56:24 -0500

    Require correct permissions

--- src/commands/config-command.ts
+++ src/commands/config-command.ts
@@ -73,7 +73,12 @@ export class ConfigCommand implements Command {
     };
     public requireDev = false;
     public requireGuild = true;
-    public requireClientPerms: PermissionString[] = ['VIEW_CHANNEL'];
+    public requireClientPerms: PermissionString[] = [
+        'ADD_REACTIONS',
+        'VIEW_CHANNEL',
+        'MANAGE_MESSAGES',
+        'READ_MESSAGE_HISTORY',
+    ];
     public requireUserPerms: PermissionString[] = [];
     public requireSetup = true;
     public requireVote = false;
