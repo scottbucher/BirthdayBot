6fe6f12c39a2f8949791722fe8508d2970904707
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-01-15 23:08:23 -0500

    Add support for buttons (#22)

--- src/start.ts
+++ src/start.ts
@@ -3,6 +3,7 @@ import { Routes } from 'discord-api-types/rest/v9';
 import { Options } from 'discord.js';
 
 import { Bot } from './bot';
+import { Button } from './buttons';
 import {
     BlacklistCommand,
     Command,
@@ -58,6 +59,7 @@ import {
     TrustedRoleRemoveRoleSubCommand,
 } from './commands/trusted-role';
 import {
+    ButtonHandler,
     CommandHandler,
     GuildJoinHandler,
     GuildLeaveHandler,
@@ -209,6 +211,11 @@ async function start(): Promise<void> {
         new VoteCommand(),
     ].sort((a, b) => (a.metadata.name > b.metadata.name ? 1 : -1));
 
+    // Buttons
+    let buttons: Button[] = [
+        // TODO: Add new buttons here
+    ];
+
     // Reactions
     let reactions: Reaction[] = [
         // TODO: Add new reactions here
@@ -221,6 +228,7 @@ async function start(): Promise<void> {
     let guildJoinHandler = new GuildJoinHandler();
     let guildLeaveHandler = new GuildLeaveHandler();
     let commandHandler = new CommandHandler(commands, subService, guildRepo, userRepo);
+    let buttonHandler = new ButtonHandler(buttons);
     let triggerHandler = new TriggerHandler(triggers);
     let messageHandler = new MessageHandler(triggerHandler);
     let reactionHandler = new ReactionHandler(reactions);
@@ -238,6 +246,7 @@ async function start(): Promise<void> {
         guildLeaveHandler,
         messageHandler,
         commandHandler,
+        buttonHandler,
         reactionHandler,
         new JobService(jobs)
     );
