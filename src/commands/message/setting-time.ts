d4e4cebce12aca7ca2de931fa42b838bd0c70d00
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-01-27 21:52:38 -0500

    Added message setting commands

---
+++ src/commands/message/setting-time.ts
@@ -0,0 +1,59 @@
+import { ApplicationCommandData, PermissionString, CommandInteraction } from 'discord.js';
+
+import { EventData } from '../../models/index.js';
+import { GuildRepo } from '../../services/database/repos/index.js';
+import { Lang } from '../../services/index.js';
+import { FormatUtils } from '../../utils/format-utils.js';
+import { MessageUtils } from '../../utils/message-utils.js';
+import { Command } from '../index.js';
+
+export class MessageSettingTimeSubCommand implements Command {
+    constructor(public guildRepo: GuildRepo) {}
+    public metadata: ApplicationCommandData = {
+        name: Lang.getCom('subCommands.add'),
+        description: undefined,
+    };
+
+    public deferType = undefined;
+    public requireDev = false;
+    public requireGuild = true;
+    public requireClientPerms: PermissionString[] = [];
+    public requireUserPerms: PermissionString[] = [];
+    public requireSetup = true;
+    public requireVote = false;
+    public requirePremium = false;
+
+    public async execute(intr: CommandInteraction, data: EventData): Promise<void> {
+        // gets the type which is either BIRTHDAY, MEMBER_ANNIVERSARY, or SERVER_ANNIVERSARY
+        let type = intr.options.getString(Lang.getCom('arguments.type'))?.toLowerCase();
+
+        let messageTime = intr.options.getInteger(Lang.getCom('arguments.time'));
+
+        let timeOutput: string;
+        if (messageTime === 0)
+            timeOutput = '12:00 ' + Lang.getRef('info', 'terms.amTime', data.lang());
+        else if (messageTime === 12)
+            timeOutput = '12:00 ' + Lang.getRef('info', 'terms.pmTime', data.lang());
+        else if (messageTime < 12)
+            timeOutput =
+                `${messageTime - 12}` + ':00 ' + Lang.getRef('info', 'terms.amTime', data.lang());
+        else
+            timeOutput =
+                `${messageTime - 12}` + ':00 ' + Lang.getRef('info', 'terms.pmTime', data.lang());
+
+        let displayType = FormatUtils.getCelebrationDisplayType(type, false).toLowerCase();
+
+        type === 'birthday'
+            ? await this.guildRepo.updateBirthdayMessageTime(intr.guild.id, messageTime)
+            : type === 'member_anniversary'
+            ? await this.guildRepo.updateMemberAnniversaryMessageTime(intr.guild.id, messageTime)
+            : await this.guildRepo.updateServerAnniversaryMessageTime(intr.guild.id, messageTime);
+        await MessageUtils.sendIntr(
+            intr,
+            Lang.getSuccessEmbed('results', 'successEmbeds.setMessageTime', data.lang(), {
+                DISPLAY_TYPE: displayType,
+                TIME: timeOutput,
+            })
+        );
+    }
+}
