bcabfa79731dc6eaeee5fe96f88e7d42f4e33b94
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-02-27 11:50:17 -0500

    Update packages

--- src/commands/test-command.ts
+++ src/commands/test-command.ts
@@ -1,4 +1,4 @@
-import { ApplicationCommandOptionType } from 'discord-api-types';
+import { ApplicationCommandOptionType } from 'discord-api-types/v9';
 import {
     ChatInputApplicationCommandData,
     CommandInteraction,
