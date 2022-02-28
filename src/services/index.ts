e29046d2a0ad602107d8d0ba4dd276df6fcebb0b
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-02-27 22:37:23 -0500

    use correct export

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,5 +3,5 @@ export { HttpService } from './http-service.js';
 export { JobService } from './job-service.js';
 export { Lang } from './lang.js';
 export { Logger } from './logger.js';
-export { MasterApiService } from './master-api-services.js';
+export { MasterApiService } from './master-api-service.js';
 export { SubscriptionService } from './subscription-service.js';
