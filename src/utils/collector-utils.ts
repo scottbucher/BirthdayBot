9d6dd73f3dd9f0177144892d55e015fb9f763bce
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-08-28 21:25:29 -0400

    Update discord.js-collector-utils

--- src/utils/collector-utils.ts
+++ src/utils/collector-utils.ts
@@ -4,7 +4,6 @@ import {
     Message,
     MessageActionRow,
     MessageEmbed,
-    MessageReaction,
     Modal,
     ModalSubmitInteraction,
     SelectMenuInteraction,
@@ -33,32 +32,27 @@ export class CollectorUtils {
     public static collectByButton<T>(
         msg: Message,
         user: User,
-        buttonRetriever: ButtonRetriever<T>,
-        expireFunction?: ExpireFunction
+        retriever: ButtonRetriever<T>,
+        expireFunc?: ExpireFunction
     ): Promise<{
         intr: ButtonInteraction;
         value: T;
     }> {
-        return DjsCollectorUtils.collectByButton(
-            msg,
-            (intr: ButtonInteraction) => intr.user.id === user.id,
-            (nextMsg: Message): boolean => {
-                if (nextMsg.author.id !== user.id) {
-                    return false;
-                }
-
+        return DjsCollectorUtils.collectByButton(msg, retriever, {
+            time: Config.experience.promptExpireTime * 1000,
+            reset: true,
+            target: user,
+            stopFilter: message => {
                 // Check if another command was ran, if so cancel the current running setup
-                let nextMsgArgs = nextMsg.content.split(' ');
-                if ([Lang.getCom('keywords.stop')].includes(nextMsgArgs[0]?.toLowerCase())) {
+                let args = message.content.split(' ');
+                if ([Lang.getCom('keywords.stop')].includes(args[0]?.toLowerCase())) {
                     return true;
                 }
 
                 return false;
             },
-            buttonRetriever,
-            expireFunction,
-            { time: Config.experience.promptExpireTime * 1000, reset: true }
-        );
+            onExpire: expireFunc,
+        });
     }
 
     public static async getBooleanFromButton(
@@ -199,120 +193,99 @@ export class CollectorUtils {
     public static collectBySelectMenu<T>(
         msg: Message,
         user: User,
-        selectMenuRetriever: SelectMenuRetriever<T>,
-        expireFunction?: ExpireFunction
+        retriever: SelectMenuRetriever<T>,
+        expireFunc?: ExpireFunction
     ): Promise<{
         intr: SelectMenuInteraction;
         value: T;
     }> {
-        return DjsCollectorUtils.collectBySelectMenu(
-            msg,
-            (intr: SelectMenuInteraction) => intr.user.id === user.id,
-            (nextMsg: Message): boolean => {
-                if (nextMsg.author.id !== user.id) {
-                    return false;
-                }
-
+        return DjsCollectorUtils.collectBySelectMenu(msg, retriever, {
+            time: Config.experience.promptExpireTime * 1000,
+            reset: true,
+            target: user,
+            stopFilter: message => {
                 // Check if another command was ran, if so cancel the current running setup
-                let nextMsgArgs = nextMsg.content.split(' ');
-                if ([Lang.getCom('keywords.stop')].includes(nextMsgArgs[0]?.toLowerCase())) {
+                let args = message.content.split(' ');
+                if ([Lang.getCom('keywords.stop')].includes(args[0]?.toLowerCase())) {
                     return true;
                 }
 
                 return false;
             },
-            selectMenuRetriever,
-            expireFunction,
-            { time: Config.experience.promptExpireTime * 1000, reset: true }
-        );
+            onExpire: expireFunc,
+        });
     }
 
     public static collectByModal<T>(
         msg: Message,
         modal: Modal,
         user: User,
-        modalRetriever: ModalRetriever<T>,
-        expireFunction?: ExpireFunction
+        retriever: ModalRetriever<T>,
+        expireFunc?: ExpireFunction
     ): Promise<{
         intr: ModalSubmitInteraction;
         value: T;
     }> {
-        return DjsCollectorUtils.collectByModal(
-            msg,
-            modal,
-            (intr: ButtonInteraction) => intr.user.id === user.id,
-            (nextMsg: Message): boolean => {
-                if (nextMsg.author.id !== user.id) {
-                    return false;
-                }
-
+        return DjsCollectorUtils.collectByModal(msg, modal, retriever, {
+            time: Config.experience.promptExpireTime * 1000,
+            reset: true,
+            target: user,
+            stopFilter: message => {
                 // Check if another command was ran, if so cancel the current running setup
-                let nextMsgArgs = nextMsg.content.split(' ');
-                if ([Lang.getCom('keywords.stop')].includes(nextMsgArgs[0]?.toLowerCase())) {
+                let args = message.content.split(' ');
+                if ([Lang.getCom('keywords.stop')].includes(args[0]?.toLowerCase())) {
                     return true;
                 }
 
                 return false;
             },
-            modalRetriever,
-            expireFunction,
-            { time: Config.experience.promptExpireTime * 1000, reset: true }
-        );
+            onExpire: expireFunc,
+        });
     }
 
     public static collectByReaction<T>(
         msg: Message,
         user: User,
-        reactionRetriever: ReactionRetriever<T>,
-        expireFunction?: ExpireFunction
+        retriever: ReactionRetriever<T>,
+        expireFunc?: ExpireFunction
     ): Promise<T> {
-        return DjsCollectorUtils.collectByReaction(
-            msg,
-            (_msgReaction: MessageReaction, reactor: User): boolean => reactor.id === user.id,
-            (nextMsg: Message): boolean => {
-                if (nextMsg.author.id !== user.id) {
-                    return false;
-                }
-
+        return DjsCollectorUtils.collectByReaction(msg, retriever, {
+            time: Config.experience.promptExpireTime * 1000,
+            reset: true,
+            target: user,
+            stopFilter: message => {
                 // Check if another command was ran, if so cancel the current running setup
-                let nextMsgArgs = nextMsg.content.split(' ');
-                if ([Lang.getCom('keywords.stop')].includes(nextMsgArgs[0]?.toLowerCase())) {
+                let args = message.content.split(' ');
+                if ([Lang.getCom('keywords.stop')].includes(args[0]?.toLowerCase())) {
                     return true;
                 }
 
                 return false;
             },
-            reactionRetriever,
-            expireFunction,
-            { time: Config.experience.promptExpireTime * 1000, reset: true }
-        );
+            onExpire: expireFunc,
+        });
     }
 
     public static collectByMessage<T>(
         channel: TextBasedChannel,
         user: User,
-        messageRetriever: MessageRetriever<T>,
-        expireFunction?: ExpireFunction
+        retriever: MessageRetriever<T>,
+        expireFunc?: ExpireFunction
     ): Promise<T> {
-        return DjsCollectorUtils.collectByMessage(
-            channel,
-            (nextMsg: Message): boolean => nextMsg.author.id === user.id,
-            (nextMsg: Message): boolean => {
-                if (nextMsg.author.id !== user.id) {
-                    return false;
-                }
-
+        return DjsCollectorUtils.collectByMessage(channel, retriever, {
+            time: Config.experience.promptExpireTime * 1000,
+            reset: true,
+            target: user,
+            stopFilter: message => {
                 // Check if another command was ran, if so cancel the current running setup
-                let nextMsgArgs = nextMsg.content.split(' ');
-                if ([Lang.getCom('keywords.stop')].includes(nextMsgArgs[0]?.toLowerCase())) {
+                let args = message.content.split(' ');
+                if ([Lang.getCom('keywords.stop')].includes(args[0]?.toLowerCase())) {
                     return true;
                 }
 
                 return false;
             },
-            messageRetriever,
-            expireFunction,
-            { time: Config.experience.promptExpireTime * 1000, reset: true }
-        );
+            onExpire: expireFunc,
+        });
     }
 }
