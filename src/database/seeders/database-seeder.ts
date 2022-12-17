77e31ed3ecc8d0f5aa5d8a05000cd1f35ff857ae
Kevin Novak <kevinmichaelnovak@gmail.com>
2022-12-16 23:39:59 -0500

    Fix arrays

--- src/database/seeders/database-seeder.ts
+++ src/database/seeders/database-seeder.ts
@@ -68,9 +68,9 @@ export class DatabaseSeeder extends Seeder {
         // Create Guild
         let guild = new GuildData(guildId, birthdayChannelDiscordId, birthdayRoleDiscordId);
         em.persist(guild);
-        guild.trustedSystemSettings.roleIds.concat([staffRole, adminRole]);
-        guild.blacklistSettings.userIds.concat([blacklistedUser1, blacklistedUser2]);
-        guild.blacklistSettings.roleIds.concat([blacklistedRole1, blacklistedRole2]);
+        guild.trustedSystemSettings.roleIds = [staffRole, adminRole];
+        guild.blacklistSettings.userIds = [blacklistedUser1, blacklistedUser2];
+        guild.blacklistSettings.roleIds = [blacklistedRole1, blacklistedRole2];
         guild.memberAnniversarySettings.channelDiscordId = memberAnniversaryChannel;
         guild.memberAnniversarySettings.memberAnniversaryRoles.push(
             new MemberAnniversaryRoleData(1, memberAnniversaryRoleYear1),
